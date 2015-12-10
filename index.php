6ac005e6f212612d8f5b0f717f9fcb74cdea33a6
nindianpd <nindi.siiplah@gmail.com>
2015-12-10 21:11:10 +0700

    perubahan kedua index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
 echo "<h1>Selamat Datang, sekarang kita belajar GITHUB</h1>";
+echo "<br>Klik disini untuk masuk halaman utama";
 ?>
---
+++ utama.php
@@ -0,0 +1,3 @@
+<?php
+echo "<h1> Ini Halaman Utama</h1>";
+?>
